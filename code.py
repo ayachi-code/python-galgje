180c3dab0c63e663ab0571567657d1dd9e99212e
bilal <buddygames.bilal@gmail.com>
2017-09-27 08:42:23 +0200

    Ik heb functies gemaakt voor dat als er iets fout is dat de hangmat completer word en heb een counter toegevoegt

---
+++ code.py
@@ -0,0 +1,60 @@
+def een():
+    print("=========")
+
+
+def twee():
+    print("=========")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+
+
+
+
+
+def drie():
+    print("=========")
+    print("|")
+    print("|")
+    print("|")
+    print("|")
+    print("==========")
+
+
+
+
+def vier():
+    print("=========")
+    print("|    |")
+    print("|    |")
+    print("|")
+    print("|")
+    print("==========")
+
+
+
+
+def vijf():
+    print("=========")
+    print("|    |")
+    print("|    |")
+    print("|    0")
+    print("|    |")
+    print("==========")
+
+
+def zes():
+    print("=========")
+    print("|    |")
+    print("|    |")
+    print("|    0")
+    print("|   /|\ ")
+    print("==========")
+
+
+
+
+woord = "bilal"
+counter = 0
+letter = input("type een letter")
