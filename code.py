8e8432dd4e3987dcd0ab35c9326c9d2d6d1c76fa
bilal <buddygames.bilal@gmail.com>
2017-09-27 09:32:08 +0200

    als je nu ? doet kan je het woord raden als het fout is dan bouwt er langzaam een hangmat als het goed is dan heb je het gehaalt

--- code.py
+++ code.py
@@ -1,7 +1,39 @@
 import hangmat
+import time
+
+
+
+def check():
+    if counter == 1:
+        hangmat.een()
+    elif counter == 2:
+        hangmat.twee()
+    elif counter == 3:
+        hangmat.drie()
+    elif counter == 4:
+        hangmat.vier()
+    elif counter == 5:
+        hangmat.vijf()
+    elif counter == 6:
+        hangmat.zes()
+
 
 
 
 woord = "bilal"
 counter = 0
-letter = input("type een letter")
+gebruikte_letters = ""
+
+
+while True:
+    letter = input("type een letter ")
+    print("deze letters heb je gebruikt: " + gebruikte_letters)
+    if letter == "?":
+         raad = input("probeer het woord te raden dan: ")
+         if raad == woord:
+             print("je hebt het woord geraden")
+             break
+         else:
+             print("test")
+             counter += 1
+             check()
